db3aa5f465330fd7f3a4bc69e8cf102b8eaddff0
burningaxe <fuckingshit>
2017-11-04 10:29:11 +0200

    Updated notifications to use channels in order to comply with Android 8.0

--- app/src/main/java/org/helpapaw/helpapaw/base/PawApplication.java
+++ app/src/main/java/org/helpapaw/helpapaw/base/PawApplication.java
@@ -6,14 +6,15 @@ import com.backendless.Backendless;
 
 import org.helpapaw.helpapaw.data.user.UserManager;
 import org.helpapaw.helpapaw.utils.Injection;
+import org.helpapaw.helpapaw.utils.NotificationUtils;
 
 /**
  * Created by iliyan on 7/25/16
  */
 public class PawApplication extends Application {
     public static final String  BACKENDLESS_APP_ID          = "***REMOVED***";
+    public static final String  BACKENDLESS_REST_API_KEY    = "***REMOVED***";
     private static final String BACKENDLESS_ANDROID_API_KEY = "***REMOVED***";
-    public static final String BACKENDLESS_REST_API_KEY    = "***REMOVED***";
 
     public static final Boolean TEST_VERSION = true;
 
@@ -24,6 +25,7 @@ public class PawApplication extends Application {
         super.onCreate();
         pawApplication = this;
         Backendless.initApp(this, BACKENDLESS_APP_ID, BACKENDLESS_ANDROID_API_KEY);
+        NotificationUtils.registerNotificationChannels(this);
 
         // This is done in order to handle the situation where user token is saved on the device but is invalidated on the server
         final UserManager userManager = Injection.getUserManagerInstance();
