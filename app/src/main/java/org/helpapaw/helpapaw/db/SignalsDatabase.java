ba5f96edc79753baeadd78c8a1589f00be3afbfb
aleksandar <spyzip@gmial.com>
2017-11-12 12:42:04 +0200

    Added db and send only notifications that are not present in the db

---
+++ app/src/main/java/org/helpapaw/helpapaw/db/SignalsDatabase.java
@@ -0,0 +1,32 @@
+package org.helpapaw.helpapaw.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import org.helpapaw.helpapaw.data.models.Signal;
+
+/**
+ * Created by Alex on 11/11/2017.
+ */
+
+@Database(entities = {Signal.class}, version = 1)
+public abstract class SignalsDatabase extends RoomDatabase {
+    private static SignalsDatabase INSTANCE;
+
+    public abstract SignalDao userDao();
+
+    public static SignalsDatabase getDatabase(Context context) {
+        if (INSTANCE == null) {
+            // allowMainThreadQueries should not be used, it is added so the query can be executed in
+            // the main thread, now we know that it is quick enough and works for now, but should be refactored!!!
+            INSTANCE = Room.databaseBuilder(context, SignalsDatabase.class, "signals_db").allowMainThreadQueries().build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+}
