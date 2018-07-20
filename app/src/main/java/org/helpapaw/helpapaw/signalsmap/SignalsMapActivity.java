9989eed68c69a1bab844452ce078808699f5f79d
Kyle <ktenzweiler@gmail.com>
2018-07-20 10:05:09 -0700

    made dialog to hold privacy policy link, added buttons to accept or decline

--- app/src/main/java/org/helpapaw/helpapaw/signalsmap/SignalsMapActivity.java
+++ app/src/main/java/org/helpapaw/helpapaw/signalsmap/SignalsMapActivity.java
@@ -1,12 +1,19 @@
 package org.helpapaw.helpapaw.signalsmap;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 import com.firebase.jobdispatcher.Constraint;
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
@@ -18,6 +25,8 @@ import com.firebase.jobdispatcher.Trigger;
 import org.helpapaw.helpapaw.R;
 import org.helpapaw.helpapaw.base.BaseActivity;
 import org.helpapaw.helpapaw.data.models.Signal;
+import org.helpapaw.helpapaw.data.user.UserManager;
+import org.helpapaw.helpapaw.utils.Injection;
 import org.helpapaw.helpapaw.utils.services.BackgroundCheckJobService;
 
 import java.util.List;
@@ -26,17 +35,76 @@ import static org.helpapaw.helpapaw.base.PawApplication.TEST_VERSION;
 
 public class SignalsMapActivity extends BaseActivity {
 
+    private SharedPreferences mSharedPreferences;
+    private final static String ACCEPTED_TERMS_CONDITIONS = "ACCEPTED_TERMS_CONDITIONS";
+    UserManager userManager;
+    private final static String URL = "https://develop.backendless.com/***REMOVED***/console/fcfdrgddsebccdkjfamuhppaasnowqluooks/files/view/web/privacypolicy.htm";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (null == savedInstanceState) {
-            if (getIntent().hasExtra(Signal.KEY_FOCUSED_SIGNAL_ID)) {
-                initFragment(SignalsMapFragment.newInstance(getIntent().getStringExtra(Signal.KEY_FOCUSED_SIGNAL_ID)));
-            } else {
-                initFragment(SignalsMapFragment.newInstance());
+
+        mSharedPreferences = getApplicationContext().getSharedPreferences(this.getClass().getSimpleName(), MODE_PRIVATE);
+        userManager = Injection.getUserManagerInstance();
+
+        if (mSharedPreferences.getBoolean(ACCEPTED_TERMS_CONDITIONS, false) ){// || !userManager.isLoggedIn()) {
+            if (null == savedInstanceState) {
+                if (getIntent().hasExtra(Signal.KEY_FOCUSED_SIGNAL_ID)) {
+                    initFragment(SignalsMapFragment.newInstance(getIntent().getStringExtra(Signal.KEY_FOCUSED_SIGNAL_ID)));
+                } else {
+                    initFragment(SignalsMapFragment.newInstance());
+                }
             }
+            scheduleBackgroundChecks();
+        } else {
+
+            final TextView message = new TextView(getApplicationContext());
+            message.setPadding(50,1,1,1);
+            // i.e.: R.string.dialog_message =>
+            // "Test this dialog following the link to dtmilano.blogspot.com"
+            final SpannableString s =
+                    new SpannableString("go to " + URL + " to read our Privacy Policy");
+            Linkify.addLinks(s, Linkify.WEB_URLS);
+            message.setText(s);
+            message.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Terms and Conditions")
+                    .setView(message)
+            .setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    if (null == savedInstanceState) {
+                        if (getIntent().hasExtra(Signal.KEY_FOCUSED_SIGNAL_ID)) {
+                            initFragment(SignalsMapFragment.newInstance(getIntent().getStringExtra(Signal.KEY_FOCUSED_SIGNAL_ID)));
+                        } else {
+                            initFragment(SignalsMapFragment.newInstance());
+                        }
+                    }
+                    scheduleBackgroundChecks();
+                }
+            })
+            .setNegativeButton("Decline", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    userManager.logout(new UserManager.LogoutCallback() {
+                        @Override
+                        public void onLogoutSuccess() {
+
+                        }
+
+                        @Override
+                        public void onLogoutFailure(String message) {
+
+                        }
+                    });
+                }
+            })
+            .show();
+
         }
-        scheduleBackgroundChecks();
+
     }
 
     @Override
