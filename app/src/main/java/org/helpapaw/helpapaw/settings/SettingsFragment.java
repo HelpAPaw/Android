d5820af8f324515af04f8b0df15bcc6d092f13bf
Andre Dublin <81dublin@gmail.com>
2018-10-20 21:50:04 -0400

    add settings activity, presenter and fragment

--- app/src/main/java/org/helpapaw/helpapaw/settings/SettingsFragment.java
+++ app/src/main/java/org/helpapaw/helpapaw/settings/SettingsFragment.java
@@ -1,4 +1,61 @@
 package org.helpapaw.helpapaw.settings;
 
-public class SettingsFragment {
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.helpapaw.helpapaw.R;
+import org.helpapaw.helpapaw.base.BaseFragment;
+import org.helpapaw.helpapaw.base.Presenter;
+import org.helpapaw.helpapaw.databinding.FragmentSettingsBinding;
+
+public class SettingsFragment extends BaseFragment implements SettingsContract.View {
+
+    private static String TAG = SettingsFragment.class.getSimpleName();
+    private SettingsPresenter mSettingsPresenter;
+
+    FragmentSettingsBinding  binding;
+
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    protected Presenter getPresenter() {
+        return mSettingsPresenter;
+    }
+
+    @Override
+    public void onRadiusChange() {
+
+    }
+
+    @Override
+    public void onTimeoutChange() {
+
+    }
 }
