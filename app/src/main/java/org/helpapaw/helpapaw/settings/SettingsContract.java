ed3b872c9ff949cd6e4f41a17b9e256293304975
Andre Dublin <81dublin@gmail.com>
2018-10-27 23:42:30 -0400

    get values from preferences and set them in the settings screen

--- app/src/main/java/org/helpapaw/helpapaw/settings/SettingsContract.java
+++ app/src/main/java/org/helpapaw/helpapaw/settings/SettingsContract.java
@@ -1,9 +1,15 @@
 package org.helpapaw.helpapaw.settings;
 
 public interface SettingsContract {
-    interface View {}
+    interface View {
+        void setRadius(int radius);
+
+        void setTimeout(int timeout);
+    }
 
     interface UserActionsListener {
+        void initialize();
+
         void onRadiusChange(int radius);
 
         void onTimeoutChange(int timeout);
