962df2a3296735c58b08577471f421a18396af3d
medeaohm <nia_omerska@abv.bg>
2021-08-11 02:42:02 +0300

    48 - edit signal - fixed bug with screen rotation

--- app/src/main/java/org/helpapaw/helpapaw/signaldetails/SignalDescriptionDialog.java
+++ app/src/main/java/org/helpapaw/helpapaw/signaldetails/SignalDescriptionDialog.java
@@ -2,6 +2,8 @@ package org.helpapaw.helpapaw.signaldetails;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -16,8 +18,10 @@ import org.helpapaw.helpapaw.data.models.Signal;
 public class SignalDescriptionDialog extends DialogFragment {
 
     public static final String EDIT_SIGNAL_DESCRIPTION_TAG = "editSignalDescription";
+    public static final String SIGNAL_TITLE = "signalTitle";
 
     private Signal signal;
+    private String signalTitle;
     private SignalDetailsPresenter presenter;
 
     public static SignalDescriptionDialog newInstance(Signal signal, SignalDetailsPresenter presenter) {
@@ -30,6 +34,15 @@ public class SignalDescriptionDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        // disable screen rotation while the dialog is open
+        int currentOrientation = getResources().getConfiguration().orientation;
+        if (currentOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+            this.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+        }
+        else {
+            this.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+        }
 
         AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
         dialog.setTitle(R.string.txt_edit_title_dialog);
@@ -53,6 +66,13 @@ public class SignalDescriptionDialog extends DialogFragment {
         });
 
         return dialog.create();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // reset the screen orientation
+        this.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER);
     }
 
     @Override
