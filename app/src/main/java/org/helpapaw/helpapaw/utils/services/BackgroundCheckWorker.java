42e696245068d9258c7680d5a66715ca47b2f7ca
medeaohm <nia_omerska@abv.bg>
2021-11-12 21:59:59 +0200

    Adding notification to db - background check worker

--- app/src/main/java/org/helpapaw/helpapaw/utils/services/BackgroundCheckWorker.java
+++ app/src/main/java/org/helpapaw/helpapaw/utils/services/BackgroundCheckWorker.java
@@ -19,9 +19,12 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import org.helpapaw.helpapaw.R;
+import org.helpapaw.helpapaw.data.models.Notification;
 import org.helpapaw.helpapaw.data.models.Signal;
 import org.helpapaw.helpapaw.data.repositories.ISettingsRepository;
 import org.helpapaw.helpapaw.data.repositories.SignalRepository;
+import org.helpapaw.helpapaw.db.NotificationsDatabase;
 import org.helpapaw.helpapaw.db.SignalsDatabase;
 import org.helpapaw.helpapaw.utils.Injection;
 import org.helpapaw.helpapaw.utils.NotificationUtils;
@@ -38,7 +41,9 @@ import static org.helpapaw.helpapaw.data.models.Signal.SOLVED;
  */
 
 public class BackgroundCheckWorker extends ListenableWorker {
-    private SignalsDatabase database;
+    private SignalsDatabase signalsDatabase;
+    private NotificationsDatabase notificationDatabase;
+
     public static final String TAG = BackgroundCheckWorker.class.getSimpleName();
     static final String CURRENT_NOTIFICATION_IDS = "CurrentNotificationIds";
 
@@ -57,7 +62,8 @@ public class BackgroundCheckWorker extends ListenableWorker {
     @NonNull
     @Override
     public ListenableFuture<ListenableWorker.Result> startWork() {
-        database = SignalsDatabase.getDatabase(getApplicationContext());
+        signalsDatabase = SignalsDatabase.getDatabase(getApplicationContext());
+        notificationDatabase = NotificationsDatabase.getDatabase(getApplicationContext());
 
         Log.d(TAG, "onStartJob called");
 
@@ -100,7 +106,7 @@ public class BackgroundCheckWorker extends ListenableWorker {
 
     @Override
     public void onStopped() {
-        database = null;
+        signalsDatabase = null;
     }
 
     private void getSignalsForLastKnownLocation(Location location, final CallbackToFutureAdapter.Completer<ListenableWorker.Result> completer) {
@@ -112,21 +118,26 @@ public class BackgroundCheckWorker extends ListenableWorker {
 
                 Log.d(TAG, "got signals");
 
-                if (signals != null && !signals.isEmpty() && database != null) {
+                if (signals != null && !signals.isEmpty() && signalsDatabase != null) {
 
                     int signalTypesSetting = settingsRepository.getSignalTypes();
 
                     for (Signal signal : signals) {
                         // Notify user only if signal is not solved and user has subscribed for that signal type
                         if ((signal.getStatus() < SOLVED) && (Utils.shouldNotifyAboutSignalType(signal.getType(), signalTypesSetting))) {
-                            List<Signal> signalsFromDB = database.signalDao().getSignal(signal.getId());
+                            List<Signal> signalsFromDB = signalsDatabase.signalDao().getSignal(signal.getId());
                             if (signalsFromDB.size() > 0) {
                                 Signal signalFromDb = signalsFromDB.get(0);
                                 if (!signalFromDb.getSeen()) {
                                     NotificationUtils.showNotificationForSignal(signal, getApplicationContext());
                                     mCurrentNotificationIds.add(signal.getId());
+
+                                    String notificationText = getApplicationContext().getString(R.string.txt_new_signal) + ": " + signal.getTitle();
+                                    Notification notification = new Notification(signal.getId(), signal.getPhotoUrl(), notificationText);
+                                    notificationDatabase.notificationDao().saveNotification(notification);
+
                                     signalFromDb.setSeen(true);
-                                    database.signalDao().saveSignal(signalFromDb);
+                                    signalsDatabase.signalDao().saveSignal(signalFromDb);
                                 }
                             }
                         }
